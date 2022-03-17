069143414156fd965435bdfbde82c55a0e774605
abdobody2040 <80859721+abdobody2040@users.noreply.github.com>
2022-03-17 19:12:02 +0200

    Nested loop training

---
+++ nested loop.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[22]:
+
+
+for line in range (2):
+    for i in range (0,10):
+        print("*",end=" ")
+    print("\n")
+    
+print("welcome to python",'\n')
+
+for line in range (2):
+    for i in range (0,10):
+        print('*',end=' ')
+    print("\n")
+
+
+# In[ ]:
+
+
+
+
