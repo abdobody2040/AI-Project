dd4ff950d757a8f4880c67c313794896f255f63d
abdobody2040 <80859721+abdobody2040@users.noreply.github.com>
2022-03-17 18:30:43 +0200

    Add files via upload

---
+++ lab 1.py
@@ -0,0 +1,15 @@
+#input no. 1
+a=input('plz enter first ')
+#input no. 2
+b=input('plz enter sec ')
+#convert to float
+a=float(a)
+b=float(b)
+#sum
+print('sum =',a+b)
+#substract
+print('sub=',a-b)
+#multi
+print('multi =',a*b)
+#devide
+print('dev = ',a/b)
---
+++ lab 2.py
@@ -0,0 +1,13 @@
+a=input('plz enter first no. ')
+b=input('plz enter sec no. ')
+op=input('plz enter operation. ')
+a=float(a)
+b=float(b)
+if op=='+' or 'sum':
+  print(a+b)
+elif op=='-' or 'sub':
+  print(a-b)
+elif op=='*' or 'x' or 'X':
+  print(a*b)
+elif op=='/' or '\\':
+  print(a/b)
